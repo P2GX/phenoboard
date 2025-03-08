1b77a97c5b9211654bd370927e900a7c90e4385b
Peter Robinson <peter.robinson@jax.org>
2025-03-08 17:52:51 +0100

    Adding routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from "@angular/router";
+import { Routes, withRouterConfig } from "@angular/router";
+import { provideRouter } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ClipboardComponent } from "./clipboard/clipboard.component";
 
@@ -6,3 +7,8 @@ export const routes: Routes = [
     { path: '', component: HomeComponent}, // default route
     { path: 'clipboard', component: ClipboardComponent },
 ];
+
+// Provide router in `main.ts`
+export const appConfig = [
+    provideRouter(routes )
+  ];
---
+++ src/app/main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withHashLocation()) // 👈 Use hash-based routing for Tauri
+  ]
+})
+  .catch(err => console.error(err));
