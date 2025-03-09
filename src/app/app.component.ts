b856251376d767337b429a9f187049911b3c146c
Peter Robinson <peter.robinson@jax.org>
2025-03-09 08:45:28 +0100

    Adding mini-tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,30 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/core";
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { TextminingComponent } from './textmining/textmining.component';
-import { SettingsComponent } from './settings/settings.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TextminingComponent, SettingsComponent, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  greetingMessage = "";
-
-  hpoText = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
-
-  
+   
 }
