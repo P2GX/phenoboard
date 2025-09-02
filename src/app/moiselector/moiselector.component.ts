6a590c2e8f85c616f807e9ef6fa649398dfdf548
Peter Robinson <peter.robinson@jax.org>
2025-09-02 06:40:21 +0200

    fixing MOI entries

--- src/app/moiselector/moiselector.component.ts
+++ src/app/moiselector/moiselector.component.ts
@@ -23,13 +23,13 @@ export class MoiSelector {
   showMoi = true;
 
   moiTerms: MoiTerm[] = [
-    { id: 'HP:0000006', label: 'Autosomal dominant', selected: false },
-    { id: 'HP:0000007', label: 'Autosomal recessive', selected: false },
+    { id: 'HP:0000006', label: 'Autosomal dominant inheritance', selected: false },
+    { id: 'HP:0000007', label: 'Autosomal recessive inheritance', selected: false },
     { id: 'HP:0001417', label: 'X-linked inheritance', selected: false },
-    { id: 'HP:0001423', label: 'X-linked dominant', selected: false },
-    { id: 'HP:0001419', label: 'X-linked recessive', selected: false },
+    { id: 'HP:0001423', label: 'X-linked dominant inheritance', selected: false },
+    { id: 'HP:0001419', label: 'X-linked recessive inheritance', selected: false },
     { id: 'HP:0001427', label: 'Mitochondrial inheritance', selected: false },
-    { id: 'HP:0032113', label: 'Digenic inheritance', selected: false },
+    { id: 'HP:0010984', label: 'Digenic inheritance', selected: false },
     { id: 'HP:0001450', label: 'Y-linked inheritance', selected: false },
   ];
 
