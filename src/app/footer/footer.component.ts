75beecf64a0420c6ec3989d765a648f0ad4b2d73
Peter Robinson <peter.robinson@jax.org>
2026-01-20 08:20:47 +0100

    Adding version to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { openUrl } from '@tauri-apps/plugin-opener';
 import { HelpService } from '../services/help.service';
+import { getVersion, getName } from '@tauri-apps/api/app';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   private helpService = inject(HelpService);
+  appVersion = '';
+  appName = '';
+
+  async ngOnInit() {
+    this.appVersion = await getVersion();
+    this.appName = await getName();
+  }
 
   async openHelp(): Promise<void> {
     const url = this.helpService.getCurrentUrlValue();   
